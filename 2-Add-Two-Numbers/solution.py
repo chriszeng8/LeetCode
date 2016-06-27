9ac91810d1a16bf8bbb7be0b6661a6214af67c15
Chris Z <chriszeng8@hotmail.com>
2016-06-27 12:20:52 -0400

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.py
+++ 2-Add-Two-Numbers/solution.py
@@ -1,14 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-    }
-};
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        carry=0
+        head=l1
+        while l1.next and l2.next:
+            l1.val,carry=(l1.val+l2.val+carry)%10,(l1.val+l2.val+carry)/10
+            l1,l2=l1.next,l2.next
+        l1.val,carry=(l1.val+l2.val+carry)%10,(l1.val+l2.val+carry)/10
+        if l2.next:
+            l1.next=l2.next
+            l1=l1.next
+        elif l1.next:
+            l1=l1.next
+        elif carry>0:
+            l1.next=ListNode(1)
+            l1.next.next=None
+            return head
+        while l1.next:
+            l1.val,carry=(l1.val+carry)%10,(l1.val+carry)/10
+            l1=l1.next
+        l1.val,carry=(l1.val+carry)%10,(l1.val+carry)/10
+        if carry>0:
+            l1.next=ListNode(1)
+            l1.next.next=None
+        return head
