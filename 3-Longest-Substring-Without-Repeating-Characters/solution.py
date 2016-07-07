173f99fb75374070d0833b999d2921a7ba5a7234
Chris Z <chriszeng8@hotmail.com>
2016-07-07 11:40:13 -0400

    [3. Longest Substring Without Repeating Characters][Accepted]committed by LeetCode Extension

--- 3-Longest-Substring-Without-Repeating-Characters/solution.py
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.py
@@ -11,26 +11,15 @@ class Solution(object):
         charDict = {}
         charDict[s[left]]=left
         LongestStrLen =1
-        tempStr=''
         while right<len(s):
         	# if the right most character is already in the hashmap
         	# currently found temporary longest substring
-        	
         	if s[right] in charDict:
         		left = max(left,charDict[s[right]]+1)
         
         	if (right-left)+1>LongestStrLen:
         		LongestStrLen=right-left+1
-        		tempStr=s[left:right+1]
         	charDict[s[right]] = right
-        	# print '=============='
-        	# print ' '*left+'|'+' '*(right-left-1)+'|'
-        	# print s
-        	# print 'right: '+str(right) + ' ' +s[right]
-        	# print charDict
-        	# print 'left-right: '+s[left:right+1]
-        	# print 'longest: '+tempStr
-        
         	right=right+1
         
         return LongestStrLen
