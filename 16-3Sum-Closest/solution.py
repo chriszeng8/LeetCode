bfec53839f0b8ff9eaac3d53819dd111cf9d4ebe
Chris Z <chriszeng8@hotmail.com>
2016-08-26 16:54:07 -0400

    [16. 3Sum Closest][Accepted]committed by LeetCode Extension

--- 16-3Sum-Closest/solution.py
+++ 16-3Sum-Closest/solution.py
@@ -7,13 +7,11 @@ class Solution(object):
         """ 
         min_diff = None
         num_len = len(nums)
+        # sort the number array
         nums.sort()
-        status = None
-        temp_diff = 0
         min_closest_diff = sys.maxint
         closest_sum = None
 
-        # print nums
         for i in xrange(num_len-2):
             j = i+1
             k = num_len-1
@@ -25,17 +23,13 @@ class Solution(object):
 
                 if (temp_sum<target):
                     j = j+1
-
                 elif (temp_sum>target):
                     k = k-1
                 else:
-                    # print "equal"
                     return temp_sum
 
                 if (min_closest_diff>abs_diff):
                     min_closest_diff = abs_diff
                     closest_sum = temp_sum
-
-                temp_diff =diff
 
         return closest_sum
